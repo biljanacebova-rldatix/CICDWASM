6962fe49ee0c09aae5bcf5fad1cbeafe035db787
Biljana Cebova <biljana.cebova@rldatix.com>
2026-04-13 14:02:27 +0200

    Tests added

---
+++ CICDWASM.Tests/HelloTest.cs
@@ -0,0 +1,25 @@
+﻿using Bunit;
+using Xunit;
+using CICDWASM.Pages;
+
+namespace CICDWASM.Tests;
+
+public class HelloTest : BunitContext
+{
+    [Fact]
+    public void RendersDefaultName()
+    {
+         var cut = Render<CICDWASM.Pages.Hello>();
+
+         cut.MarkupMatches("<h3>Hello World</h3>");
+    }
+
+    [Fact]
+    public void RendersPassedParameter()
+    {
+        var cut = Render<Hello>(parameters =>
+            parameters.Add(p => p.Name, "Alice"));
+
+        cut.MarkupMatches("<h3>Hello Alice</h3>");
+    }
+}
